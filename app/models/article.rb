a4bc4486e1dfb1b9513766e305218820436292a5
MD <readloop1@gmail.com>
2018-05-13 18:09:30 +0000

    New template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   
-  validates :title, presence: true, lenght: {minimum: 3, maximum: 50}
-  validates :description, presence: true, lenght: {minimum: 10, maximum: 50}
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 5, maximum: 50}
   
   
 end
